4d0bfe75baa9ef1698e038c398097bbd8c704669
Shahadat Hossain <shahadat15-13593@diu.edu.bd>
2022-03-22 20:41:41 +0600

    Number data type

---
+++ Chapter_1/5.numbers_data/sandbox.js
@@ -0,0 +1,64 @@
+let radius = 10;
+const pi =3.1416;
+
+console.log(radius, pi);
+
+
+console.log(10 / 2);
+
+
+let result =  radius % 3;
+console.log(result);
+
+
+
+let result1 = pi  * radius ** 2;
+console.log(result1);
+
+
+// oder of operation - B I D M A S 
+
+let number = 5 * (10 - 3) ** 2 ;
+console.log(number);
+
+
+
+let likes = 10 ;
+likes = likes + 1;
+console.log(likes);
+
+
+// another way to increase variable value
+
+likes++;
+console.log(likes);
+
+// or we can  decrease value 
+
+likes--;
+console.log(likes);
+
+
+likes += 10;
+console.log(likes);
+
+likes -= 10;
+console.log(likes);
+
+likes /= 10;
+console.log(likes);
+
+likes *= 10;
+console.log(likes);
+
+
+
+// Not a number -- NaN
+
+console.log(5 / 'hello');
+console.log(5 * 'hello');
+
+// concatenate number
+
+let result4 = 'The blog has ' + likes + ' Likes';
+console.log(result4);
