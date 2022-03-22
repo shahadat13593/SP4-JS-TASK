deba8d8a7bde8379a15d633b7949950d8f8df097
Shahadat Hossain <shahadat15-13593@diu.edu.bd>
2022-03-22 20:14:28 +0600

    string data type update

---
+++ Chapter_1/4_String_methods/sandbox.js
@@ -0,0 +1,26 @@
+// Common String Methods
+
+
+let email = 'shahadat15-13593@diu.edu.bd';
+
+// find the last index
+let index = email.lastIndexOf('a');
+console.log(index);
+
+// slice
+let  index2 = email.slice(0,8);
+console.log(index2);
+
+
+// substring
+
+let index3 = email.substr(4,10);
+console.log(index3);
+
+
+
+
+// Replace character
+
+let index4 = email.replace('s', 'r');
+console.log(index4);
