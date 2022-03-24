6d0529a366cf665e42e252eff50f4526df87506f
Shahadat Hossain <shahadat15-13593@diu.edu.bd>
2022-03-24 18:24:54 +0600

    for loop

---
+++ Chapter_2/1.1 For loops/sandbox.js
@@ -0,0 +1,30 @@
+// For loop
+
+for(let i = 0; i < 5; i++){
+    console.log('in loop:',i);
+
+}
+
+console.log('Loop finished:');
+
+console.log('===============================');
+
+const names = ['shahadat', 'Hossain', 'raiyan'];
+
+for(let i = 0; i < names.length; i++){
+    console.log(names[i]); 
+}
+console.log('===============================');
+
+
+
+for(let i = 0; i < names.length; i++){
+  let html = `<div> ${names[i]} </div>`;
+  console.log(html);
+
+}
+
+console.log('===============================');
+
+
+
