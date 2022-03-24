63ad4d599d19ded0e04fd564c7d47382b1f2c01b
Shahadat Hossain <shahadat15-13593@diu.edu.bd>
2022-03-24 19:41:39 +0600

    Do while loop

---
+++ Chapter_2/1.3 Do While/sandbox.js
@@ -0,0 +1,17 @@
+let i = 0;
+do{
+    console.log('in loop: ',i)
+    i++;
+    
+    }while(i < 5 );
+
+console.log('===============================');
+
+
+i = 0
+const names = ['shahadat', 'Hossain', 'raiyan'];
+do {
+    console.log(names[i])
+    i++;
+    
+    }while(i < names.length);
