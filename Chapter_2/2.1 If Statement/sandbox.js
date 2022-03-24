d1d98893291b751c8c0feae7f424f1c14c38d2f9
Shahadat Hossain <shahadat15-13593@diu.edu.bd>
2022-03-24 20:22:44 +0600

    Else if statement

--- Chapter_2/2.1 If Statement/sandbox.js
+++ Chapter_2/2.1 If Statement/sandbox.js
@@ -1,4 +1,10 @@
-const password = 'pass12456';
-if(password.length > 8 ){
-    console.log("This password is long enough")
-}
+const password = 'pass123456766';
+
+if(password.length >= 12 ) {
+    console.log("This password is so strong.");
+} else if(password.length >= 8 ) {
+    console.log("This password is long enough.");
+} else {
+    console.log("This password is not long enough.");
+
+ }
---
+++ Chapter_2/2.2 else If Statement/sandbox.js
@@ -0,0 +1,4 @@
+const password = 'pass12456';
+if(password.length > 8 ){
+    console.log("This password is long enough")
+}
