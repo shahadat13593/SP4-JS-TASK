6624bf8c5e6e15178ccd09291ed696f1d71225f0
Shahadat Hossain <shahadat15-13593@diu.edu.bd>
2022-03-26 01:08:23 +0600

    switch statement

---
+++ Chapter_2/5 Switch statement/sandbox.js
@@ -0,0 +1,23 @@
+const grade = 'A';
+
+switch(grade){
+
+    case 'A':
+        console.log('You got and A!.');
+        break;
+    case 'B':
+        console.log('You got and B!.');
+        break;
+     case 'C':
+        console.log('You got and C!.');
+        break;
+     case 'D':
+         console.log('You got and D!.');
+         break;
+    case 'E':
+         console.log('You got and E!.');
+         break;
+    default:
+        console.log('Not a valid grade.');
+
+}
