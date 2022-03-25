0a111e7042ed9cebb5f09bf99204cc45400b0c3d
Shahadat Hossain <shahadat15-13593@diu.edu.bd>
2022-03-26 00:07:46 +0600

    Logical operator

--- Chapter_2/3 Logical Operator/sandbox.js
+++ Chapter_2/3 Logical Operator/sandbox.js
@@ -1,1 +1,22 @@
 // Logical operator - or || and and &&
+const password = 'pass12766';
+
+if(password.length >= 12 && password.includes('@') ) {
+    console.log("This password is so strong.");
+} else if(password.length >= 8 || password.includes('@') ) {
+    console.log("This password is long enough.");
+} else {
+    console.log("This password is not long enough.");
+
+ }
+
+ // Logical Not (!)
+
+ let user = false;
+ if(! user){
+     console.log("You must be logged in to continue.");
+
+ }
+
+ console.log(!true);
+ console.log(!false);
