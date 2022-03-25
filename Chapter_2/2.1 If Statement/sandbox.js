44042e8cc001daf0853a57976ea425e98d8c3ffa
Shahadat Hossain <shahadat15-13593@diu.edu.bd>
2022-03-25 23:58:54 +0600

    if else

--- Chapter_2/2.1 If Statement/sandbox.js
+++ Chapter_2/2.1 If Statement/sandbox.js
@@ -2,9 +2,5 @@ const password = 'pass123456766';
 
 if(password.length >= 12 ) {
     console.log("This password is so strong.");
-} else if(password.length >= 8 ) {
-    console.log("This password is long enough.");
-} else {
-    console.log("This password is not long enough.");
 
  }
