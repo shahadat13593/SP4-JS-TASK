e8a79547762b08249aa76e5dfc07320e249aefb1
shahadat13593 <shahadat15-13593@diu.edu.bd>
2022-03-26 21:57:04 +0600

    Methods

---
+++ Chapter_3/2 Method/sandbox.js
@@ -0,0 +1,12 @@
+const names = 'Shahadat';
+
+// function
+
+const greet = () => 'hello';
+let resultOne = greet();
+console.log(resultOne);
+
+// methods
+
+let resultTwo = names.toUpperCase();
+console.log(resultTwo)
