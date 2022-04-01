e6be31bb7ddf32eb860e1f77c387c2cdf5ad4e53
shahadat13593 <shahadat15-13593@diu.edu.bd>
2022-04-01 12:58:24 +0600

    Methods

---
+++ Chapter_4/1.1 Add methods to object/sandbox.js
@@ -0,0 +1,28 @@
+
+let user = {
+    name : 'Shahadat', 
+    age: 19,
+    email: "shahadat15-13593@diu.ed.bd",
+    location: 'Bnagladesh',
+    blog: ['why mac & cheese rules', '10 things to make with marmite'],
+    login: function(){
+        console.log('The user logged in.');
+
+    },
+
+    logout:function(){
+        console.log('The user logged out.');
+
+      
+    }
+  
+
+
+}; 
+
+user.login();
+
+user.logout();
+// const Name = 'Shahadat';
+// Name.toUpperCase();
+// console.log(Name);
