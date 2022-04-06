1baef69688e6db8265f0dd5694f1c182162541b0
shahadat13593 <shahadat15-13593@diu.edu.bd>
2022-04-06 18:13:20 +0600

    Get and update attribute

---
+++ Chapter_5/5 Method for set attribute/sandbox.js
@@ -0,0 +1,11 @@
+const title = document.querySelector('h1');
+
+console.log(title.style);
+console.log(title.style.color);
+
+title.style.margin = '50px';
+title.style.color = 'crimson';
+
+title.style.fontSize = '50px';
+title.style.margin = "";
+
