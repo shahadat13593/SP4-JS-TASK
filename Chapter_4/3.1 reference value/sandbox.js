9b4777de16d2f54ba47b421ac80dc8c454be496b
shahadat13593 <shahadat15-13593@diu.edu.bd>
2022-04-06 13:18:31 +0600

    reference value

---
+++ Chapter_4/3.1 reference value/sandbox.js
@@ -0,0 +1,14 @@
+
+// referance values
+
+const userOne = { name: 'shahadat', age: 30};
+userTwo = userOne;
+
+console.log(userOne, userTwo);
+
+userOne.name = 'raiyan';
+
+userOne.age = 40;
+
+console.log(userOne, userTwo);
+
